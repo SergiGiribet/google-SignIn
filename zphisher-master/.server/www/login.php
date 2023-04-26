c120eff1e214f271e0d797d7c1809f67ec0ee86d
Geriijacki <gerardloriz05@gmail.com>
2023-04-26 17:57:06 +0200

    server 100% operatiu

--- zphisher-master/.server/www/login.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-//file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . "\n", FILE_APPEND);
-header('Location: password.php?username='.$_POST['username']);
-exit();
-
-?>
